0d06fdd3cb221b7fddd08ef77bcf1d8442ae734b
gasytek_cp <SND\gasytek_cp>
2012-05-18 20:27:48 +0000

    - Minor refactorings

--- Main/GasyTek.Lakana/GasyTek.Lakana.WPF/Services/NavigationInfo.cs
+++ Main/GasyTek.Lakana/GasyTek.Lakana.WPF/Services/NavigationInfo.cs
@@ -26,15 +26,15 @@
         /// <value>
         /// 	<c>true</c> if the view will appear iin <see cref="INavigationService.OpenedViews"/>; otherwise, <c>false</c>.
         /// </value>
-        public bool ShowAsOpenedView { get; private set; }
+        public bool IsOpenedViewMember { get; private set; }
 
-        private NavigationInfo(string viewKey, string parentViewKey, object viewModel, bool showAsOpenedView)
+        private NavigationInfo(string viewKey, string parentViewKey, object viewModel, bool isOpenedViewMember)
             : this()
         {
             ViewKey = viewKey;
             ParentViewKey = parentViewKey;
             ViewModel = viewModel;
-            ShowAsOpenedView = showAsOpenedView;
+            IsOpenedViewMember = isOpenedViewMember;
         }
 
         #region Factory methods
