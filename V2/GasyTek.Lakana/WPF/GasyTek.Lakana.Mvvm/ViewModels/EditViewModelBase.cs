8c6c3e84e5ae29f56f93a2de2fb246a789475594
gasytek_cp <SND\gasytek_cp>
2012-08-25 13:51:47 +0000

    - Clean up code

--- V2/GasyTek.Lakana/WPF/GasyTek.Lakana.Mvvm/ViewModels/EditViewModelBase.cs
+++ V2/GasyTek.Lakana/WPF/GasyTek.Lakana.Mvvm/ViewModels/EditViewModelBase.cs
@@ -80,65 +80,7 @@ namespace GasyTek.Lakana.Mvvm.ViewModels
 
         #endregion
 
-        //#region Validation rule management
-
-        ///// <summary>
-        ///// Initial method to use when defining the rules for a property.
-        ///// </summary>
-        ///// <param name="propertyExpression">The property expression.(ex : vm => vm.MyProperty ) </param>
-        ///// <returns></returns>
-        //protected IPropertyDefitionPart ForProperty(Expression<Func<TViewModel, IViewModelProperty>> propertyExpression) 
-        //{
-        //    var viewModelProperty = ExtractViewModelPropertyInfo(propertyExpression).PropertyInstance;
-        //    return new PropertyDefinitionPart(viewModelProperty);
-        //}
-
-        ///// <summary>
-        ///// Initial method to use when defining the rules for many related properties. 
-        ///// Those rules are global rules for the object, that is they concern many related properties.
-        ///// </summary>
-        ///// <typeparam name="TViewModel">The type of the view model.</typeparam>
-        ///// <param name="propertyExpression">The default property expression. eg : vm => vm.MyProperty</param>
-        ///// <param name="propertyExpressions">Other properties on which to attach the rule.</param>
-        ///// <returns></returns>
-        //protected IPropertyGroupDefinitionPart<TViewModel> ForProperties(Expression<Func<TViewModel, IViewModelProperty>> propertyExpression,
-        //            params Expression<Func<TViewModel, IViewModelProperty>> [] propertyExpressions)
-        //{
-        //    IEnumerable<Expression<Func<TViewModel, IViewModelProperty>>> allPropertyExpressions 
-        //        = new List<Expression<Func<TViewModel, IViewModelProperty>>> { propertyExpression };
-        //    allPropertyExpressions = allPropertyExpressions.Concat(propertyExpressions);
-            
-        //    var ownerViewModelProperties = allPropertyExpressions.Select(ExtractViewModelPropertyInfo);
-        //    return new PropertyGroupDefinitionPart<TViewModel>((TViewModel)(object)this, ownerViewModelProperties);
-        //}
-
-        //private ViewModelPropertyInfo ExtractViewModelPropertyInfo(Expression<Func<TViewModel, IViewModelProperty>> propertyExpression)
-        //{
-        //    if (propertyExpression == null)
-        //        throw new ArgumentNullException("propertyExpression", "propertyExpression is null.");
-
-        //    var memberExpr = propertyExpression.Body as MemberExpression;
-        //    if (memberExpr != null && memberExpr.Member.MemberType == MemberTypes.Property)
-        //    {
-        //        var propertyName = memberExpr.Member.Name;
-        //        if (string.IsNullOrEmpty(propertyName))
-        //            throw new InvalidOperationException("The property name was null");
-        //        var propertyInfo = GetType().GetProperty(propertyName);
-        //        if (propertyInfo == null)
-        //            throw new InvalidOperationException(string.Format("The property with name '{0}' was not found on '{1}'", propertyName, GetType()));
-        //        var viewModelProperty = propertyInfo.GetValue(this, null) as IViewModelProperty;
-        //        if (viewModelProperty == null)
-        //            throw new InvalidOperationException("Only property of type IViewModelProperty are allowed");
-
-        //        return new ViewModelPropertyInfo { PropertyName = propertyName, PropertyInstance = viewModelProperty};
-        //    }
-
-        //    throw new ArgumentException("No property reference expression was found.", "propertyExpression");
-        //}
-
-        //#endregion
-
-        protected sealed override void OnCreateCommands()
+        protected override void OnCreateCommands()
         {
             _saveCommand = CreateCommand(OnSaveCommandExecute, OnSaveCommandCanExecute);
             _cancelCommand = CreateCommand(OnCancelCommandExecute);
