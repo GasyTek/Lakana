6ca8fe6abd46dc3eb34b679aaf6b9e4da0870952
gasytek_cp <9196424+GasyTek@users.noreply.github.com>
2012-12-25 11:26:50 +0000

    - UNSTABLE VERSION : make the previous chekin to compile

---
+++ V2/GasyTek.Lakana/WPF/GasyTek.Lakana.Navigation.Tests/NavigationManagerFixture.cs
@@ -0,0 +1,443 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using GasyTek.Lakana.Navigation.Controls;
+using GasyTek.Lakana.Navigation.Services;
+using GasyTek.Lakana.Navigation.Tests.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GasyTek.Lakana.Navigation.Tests
+{
+    [TestClass]
+    public class NavigationManagerFixture
+    {
+        private NavigationManagerImpl _navigationManagerImpl;
+
+        [TestInitialize]
+        public void OnSetup()
+        {
+            _navigationManagerImpl = new NavigationManagerImpl(new FakeViewLocator());
+            _navigationManagerImpl.SetMainWorkspace(new Grid(), new FakeWorkspaceAdapter());
+        }
+
+        [TestClass]
+        public class NavigateTo : NavigationManagerFixture
+        {
+            [TestMethod]
+            public void CanNavigateToNewView()
+            {
+                // Prepare
+
+                // Act
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("view1");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.AreEqual(expectedViewInfo.ViewInstanceKey, "view1");
+                Assert.IsTrue(_navigationManagerImpl.NbTotalViews == 1);
+            }
+
+            [TestMethod]
+            public void CanNavigateToNewParameterizedView()
+            {
+                // Prepare
+
+                // Act
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("view1#1");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.AreEqual(expectedViewInfo.ViewInstanceKey, "view1#1");
+                Assert.IsTrue(_navigationManagerImpl.NbTotalViews == 1);
+            }
+
+            [TestMethod]
+            public void CanNavigateToExistingView()
+            {
+                // Prepare
+                _navigationManagerImpl.NavigateTo("view1");
+
+                // Act
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("view1");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.AreEqual(expectedViewInfo.ViewInstanceKey, "view1");
+                Assert.IsTrue(_navigationManagerImpl.NbTotalViews == 1);
+            }
+
+            [TestMethod]
+            public void CanNavigateToExistingParameterizedView()
+            {
+                // Prepare
+                _navigationManagerImpl.NavigateTo("view1#1");
+
+                // Act
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("view1#1");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.AreEqual(expectedViewInfo.ViewInstanceKey, "view1#1");
+                Assert.IsTrue(_navigationManagerImpl.NbTotalViews == 1);
+            }
+
+            [TestMethod]
+            public void CanNavigateToHiddenView()
+            {
+                // Prepare
+                _navigationManagerImpl.NavigateTo("view1");
+                _navigationManagerImpl.NavigateTo("view2");
+
+                // Act
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("view1");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.AreEqual(expectedViewInfo.ViewInstanceKey, "view1");
+                Assert.IsTrue(_navigationManagerImpl.NbTotalViews == 2);
+            }
+
+            [TestMethod]
+            public void CanStackNewViewOnExistingView()
+            {
+                // Prepare
+                var parentViewInfo = _navigationManagerImpl.NavigateTo("parentView1");
+
+                // Act
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("parentView1/view1");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.IsTrue(_navigationManagerImpl.NbTotalViews == 2);
+                Assert.IsNotNull(_navigationManagerImpl.ActiveNode.Previous);
+                Assert.AreSame(_navigationManagerImpl.ActiveNode.Previous.Value.ViewInstance, parentViewInfo.ViewInstance);
+            }
+
+            [TestMethod]
+            public void CanNavigateToHiddenStackedView()
+            {
+                // Prepare
+                _navigationManagerImpl.NavigateTo("parentView1");
+                _navigationManagerImpl.NavigateTo("parentView1/view1");
+                _navigationManagerImpl.NavigateTo("view2");
+
+                // Act
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("parentView1/view1");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.AreEqual(expectedViewInfo.ViewInstanceKey, "view1");
+                Assert.IsTrue(_navigationManagerImpl.NbTotalViews == 3);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ParentViewNotFoundException))]
+            public void CannotStackNewViewOnNotExistingView()
+            {
+                // Prepare
+
+                // Act
+                _navigationManagerImpl.NavigateTo("parentView1/view1");
+
+                // Verify
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(OnlyNewViewCanBeStackedException))]
+            public void CannotStackExistingViewOnDifferentParents()
+            {
+                // Prepare
+                _navigationManagerImpl.NavigateTo("parentView1");
+                _navigationManagerImpl.NavigateTo("parentView1/view1");
+                _navigationManagerImpl.NavigateTo("parentView2");
+
+                // Act
+                _navigationManagerImpl.NavigateTo("parentView2/view1");
+
+                // Verify
+            }
+
+            [TestMethod]
+            public void CanStackNewParameterizedViewOnExistingView()
+            {
+                // Prepare
+                var parentViewInfo = _navigationManagerImpl.NavigateTo("parentView1");
+
+                // Act
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("parentView1/view1#1");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.IsTrue(_navigationManagerImpl.NbTotalViews == 2);
+                Assert.IsNotNull(_navigationManagerImpl.ActiveNode.Previous);
+                Assert.AreSame(_navigationManagerImpl.ActiveNode.Previous.Value.ViewInstance, parentViewInfo.ViewInstance);
+            }
+
+            [TestMethod]
+            public void CanNavigateMoreThanOnceToSameView()
+            {
+                // Prepare
+                _navigationManagerImpl.NavigateTo("view1");
+                _navigationManagerImpl.NavigateTo("view1");
+
+                // Act
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("view1");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.IsTrue(_navigationManagerImpl.NbTotalViews == 1);
+            }
+
+            [TestMethod]
+            public void CanNavigateMoreThanOnceToSameStackedView()
+            {
+                // Prepare
+                var parentViewInfo = _navigationManagerImpl.NavigateTo("parentView1");
+                _navigationManagerImpl.NavigateTo("parentView1/view1");
+                _navigationManagerImpl.NavigateTo("parentView1/view1");
+
+                // Act
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("parentView1/view1");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.IsTrue(_navigationManagerImpl.NbTotalViews == 2);
+                Assert.IsNotNull(_navigationManagerImpl.ActiveNode.Previous);
+                Assert.AreSame(_navigationManagerImpl.ActiveNode.Previous.Value.ViewInstance, parentViewInfo.ViewInstance);
+            }
+
+            [ExpectedException(typeof(ParentViewNotTopMostException))]
+            [TestMethod]
+            public void CannotStackNewViewOnNotTopMostView()
+            {
+                // Prepare
+                _navigationManagerImpl.NavigateTo("parentView1");
+                _navigationManagerImpl.NavigateTo("parentView1/view1");
+
+                // Act
+                _navigationManagerImpl.NavigateTo("parentView1/view2");
+
+                // Verify
+            }
+
+            [TestMethod]
+            public void CanNavigateToNotTopMostView()
+            {
+                // Prepare
+                _navigationManagerImpl.NavigateTo("parentView1");
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("parentView1/view1");
+                _navigationManagerImpl.NavigateTo("view2");
+
+                // Act
+                _navigationManagerImpl.NavigateTo("parentView1");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.IsTrue(_navigationManagerImpl.NbTotalViews == 3);
+            }
+
+            [TestMethod]
+            public void CanNavigateToNewViewWithViewModel()
+            {
+                // Prepare
+                var fakeViewModel = new FakeViewModel();
+
+                // Act
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("view1", fakeViewModel);
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance.DataContext, fakeViewModel);
+            }
+
+            #region ViewKeyAware support
+
+            [TestMethod]
+            public void CanSupportViewKeyAwareViewModel()
+            {
+                // Prepare
+                var fakeViewKeyAwareViewModel = new FakeViewKeyAwareViewModel();
+
+                // Act
+                _navigationManagerImpl.NavigateTo("view1", fakeViewKeyAwareViewModel);
+
+                // Verify
+                Assert.AreEqual("view1", fakeViewKeyAwareViewModel.ViewInstanceKey);
+            }
+
+            [TestMethod]
+            public void CanSupportViewKeyAwareViewModelForStackedView()
+            {
+                // Prepare
+                var fakeViewKeyAwareViewModel = new FakeViewKeyAwareViewModel();
+                _navigationManagerImpl.NavigateTo("parentView1");
+
+                // Act
+                _navigationManagerImpl.NavigateTo("parentView1/view1", fakeViewKeyAwareViewModel);
+
+                // Verify
+                Assert.AreEqual("view1", fakeViewKeyAwareViewModel.ViewInstanceKey);
+            }
+
+            #endregion
+
+
+        }
+
+        [TestClass]
+        public class ShowModal : NavigationManagerFixture
+        {
+            [TestMethod]
+            public void CanShowModal()
+            {
+                // Prepare
+                var parentViewInfo = _navigationManagerImpl.NavigateTo("parentView1");
+
+                // Act
+                var expectedResult = _navigationManagerImpl.ShowModal<object>("parentView1/view1");
+
+                // Verify
+                Assert.IsTrue(expectedResult.ViewInfo.IsModal);
+                Assert.AreSame(expectedResult.ViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.IsInstanceOfType(_navigationManagerImpl.ActiveView.ViewInstance, expectedResult.ViewInfo.ViewInstance.GetType());
+                Assert.IsInstanceOfType(_navigationManagerImpl.ActiveView.InternalViewInstance, typeof(ModalHostControl));
+                Assert.IsNotNull(_navigationManagerImpl.ActiveNode.Previous);
+                Assert.AreSame(_navigationManagerImpl.ActiveNode.Previous.Value.ViewInstance, parentViewInfo.ViewInstance);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(NavigationKeyFormatException))]
+            public void CannotAcceptNavigationKeyBadFormat()
+            {
+                // Prepare
+
+                // Act
+                _navigationManagerImpl.ShowModal<object>("view1");
+
+                // Verify
+            }
+
+            [TestMethod]
+            public void CanRetrieveModalResult()
+            {
+                // Prepare
+                _navigationManagerImpl.NavigateTo("parentView1");
+
+                // Act
+                var modalResult = _navigationManagerImpl.ShowModal<string>("parentView1/view1");
+                _navigationManagerImpl.Close(modalResult.ViewInfo.ViewInstanceKey, "-- Result --");  // Close the modal view and provide the modal result
+
+                // Verify
+                Assert.AreEqual("-- Result --", modalResult.AsyncResult.Result);
+            }
+        }
+
+        [TestClass]
+        public class ShowMessageBox : NavigationManagerFixture
+        {
+            [TestMethod]
+            public void CanShowMessageBoxDialog()
+            {
+                // Prepare
+                var parentViewInfo = _navigationManagerImpl.NavigateTo("view1");
+
+                // Act
+                _navigationManagerImpl.ShowMessageBox("view1", "Lorem ipsum", MessageBoxImage.Warning, MessageBoxButton.OKCancel);
+
+                // Verify
+                Assert.IsTrue(_navigationManagerImpl.ActiveView.IsModal);
+                Assert.IsTrue(_navigationManagerImpl.ActiveView.IsMessageBox);
+                Assert.IsInstanceOfType(_navigationManagerImpl.ActiveView.ViewInstance, typeof(MessageBoxControl));
+                Assert.IsInstanceOfType(_navigationManagerImpl.ActiveView.InternalViewInstance, typeof(ModalHostControl));
+                Assert.IsNotNull(_navigationManagerImpl.ActiveNode.Previous);
+                Assert.AreSame(_navigationManagerImpl.ActiveNode.Previous.Value.ViewInstance, parentViewInfo.ViewInstance);
+            }
+
+
+        }
+
+        [TestClass]
+        public class Close : NavigationManagerFixture
+        {
+            [TestMethod]
+            public void CanCloseView()
+            {
+                // Prepare
+                var viewInfo = _navigationManagerImpl.NavigateTo("view1");
+
+                // Act
+                var closedViewInfo = _navigationManagerImpl.Close("view1");
+
+                // Verify
+                Assert.IsTrue(_navigationManagerImpl.NbTotalViews == 0);
+                Assert.AreSame(viewInfo.ViewInstance, closedViewInfo.ViewInstance);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(CannotCloseNotTopMostViewException))]
+            public void CannotCloseNotTopMostView()
+            {
+                // Prepare
+                _navigationManagerImpl.NavigateTo("parentView1");
+                _navigationManagerImpl.NavigateTo("parentView1/view1");
+
+                // Act
+                _navigationManagerImpl.Close("parentView1");
+
+                // Verify
+            }
+
+            [TestMethod]
+            public void CanRestoreLastViewWhenActiveViewIsClosed()
+            {
+                // Prepare
+                _navigationManagerImpl.NavigateTo("view2");
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("view1");
+                _navigationManagerImpl.NavigateTo("view3");
+
+                // Act
+                _navigationManagerImpl.Close("view3");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+                Assert.AreEqual(expectedViewInfo.ViewInstanceKey, "view1");
+            }
+
+            [TestMethod]
+            public void CanKeepActiveViewWhenHiddenViewIsClosed()
+            {
+                // Prepare
+                _navigationManagerImpl.NavigateTo("view2");
+                _navigationManagerImpl.NavigateTo("view1");
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("view3");
+
+                // Act
+                _navigationManagerImpl.Close("view2");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+            }
+
+            [TestMethod]
+            public void CanRestoreParentViewWhenChildViewIsClosed()
+            {
+                // Prepare
+                var expectedViewInfo = _navigationManagerImpl.NavigateTo("parentView1");
+                _navigationManagerImpl.NavigateTo("parentView1/view1");
+
+                // Act
+                _navigationManagerImpl.Close("view1");
+
+                // Verify
+                Assert.AreSame(expectedViewInfo.ViewInstance, _navigationManagerImpl.ActiveView.ViewInstance);
+            }
+
+
+
+        }
+
+        [TestClass]
+        public class CloseApplication : NavigationManagerFixture
+        {
+
+        }
+    }
+}
---
+++ V2/GasyTek.Lakana/WPF/GasyTek.Lakana.Navigation/Services/INavigationManager.cs
@@ -0,0 +1,61 @@
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GasyTek.Lakana.Navigation.Services
+{
+    /// <summary>
+    /// Represents a navigation manager system.
+    /// </summary>
+    internal interface INavigationManager
+    {
+        /// <summary>
+        /// Navigates to.
+        /// </summary>
+        /// <param name="navigationKey">The navigation key.</param>
+        /// <returns></returns>
+        ViewInfo NavigateTo(string navigationKey);
+
+        /// <summary>
+        /// Navigates to.
+        /// </summary>
+        /// <param name="navigationKey">The navigation key.</param>
+        /// <param name="viewModel">The view model.</param>
+        /// <returns></returns>
+        ViewInfo NavigateTo(string navigationKey, object viewModel);
+
+        /// <summary>
+        /// Shows the modal.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="navigationKey">The navigation key.</param>
+        /// <returns></returns>
+        ModalResult<TResult> ShowModal<TResult>(string navigationKey);
+
+        /// <summary>
+        /// Shows the message box.
+        /// </summary>
+        /// <param name="ownerViewKey">The parent instance key.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="messageBoxImage">The message box image.</param>
+        /// <param name="messageBoxButton">The message box button.</param>
+        /// <returns></returns>
+        Task<MessageBoxResult> ShowMessageBox(string ownerViewKey, string message = "",
+                                              MessageBoxImage messageBoxImage = MessageBoxImage.Information,
+                                              MessageBoxButton messageBoxButton = MessageBoxButton.OK);
+
+        /// <summary>
+        /// Closes the specified view key.
+        /// </summary>
+        /// <param name="viewKey">The view key.</param>
+        /// <param name="modalResult">The modal result.</param>
+        /// <returns></returns>
+        ViewInfo Close(string viewKey, object modalResult = null);
+
+        /// <summary>
+        /// Closes the application.
+        /// </summary>
+        /// <param name="forceClose">if set to <c>true</c> [force close].</param>
+        /// <returns></returns>
+        bool CloseApplication(bool forceClose = false);
+    }
+}
