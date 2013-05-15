dd11584cccd1328027d5cbfe3edd5b8ba2c279e7
gasytek_cp <SND\gasytek_cp>
2013-05-15 07:56:14 +0000

    - First implementation of a message bus for Lakana applications

---
+++ V2/GasyTek.Lakana/WPF/GasyTek.Lakana.Common/Communication/Message.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GasyTek.Lakana.Common.Communication
+{
+    /// <summary>
+    /// Base class for all messages that will be exchanged through the application.
+    /// </summary>
+    public abstract class Message
+    {
+        /// <summary>
+        /// Gets the original sender of the message.
+        /// </summary>
+        public object Sender
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Message"/> class.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        protected Message(object sender)
+        {
+            Sender = sender;
+        }
+    }
+}
---
+++ V2/GasyTek.Lakana/WPF/GasyTek.Lakana.Common/Communication/MessageBus.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GasyTek.Lakana.Common.Communication
+{
+    /// <summary>
+    /// Singleton object that represents a message bus for the application.
+    /// Views and View models communicate messages via the message bus.
+    /// </summary>
+    public static class MessageBus
+    {
+        private static MessageBusImpl _messageBusImpl;
+
+        static MessageBus()
+        {
+            _messageBusImpl = new MessageBusImpl();
+        }
+
+        /// <summary>
+        /// Publishes the specified message.
+        /// </summary>
+        /// <typeparam name="TMessage">The type of the message.</typeparam>
+        /// <param name="message">The message.</param>
+        public static void Publish<TMessage>(TMessage message) where TMessage : Message
+        {
+            _messageBusImpl.Publish(typeof(TMessage), message);
+        }
+
+        /// <summary>
+        /// Subscribes to the specified message.
+        /// </summary>
+        /// <typeparam name="TMessage">The type of the message.</typeparam>
+        /// <param name="onMessageReceived">The callback to use to process the message.</param>
+        /// <returns></returns>
+        public static IDisposable Subscribe<TMessage>(Action<TMessage> onMessageReceived) where TMessage : Message
+        {
+            return _messageBusImpl.Subscribe(typeof(TMessage), (Action<Message>)onMessageReceived);
+        }
+
+        #region Private class MessageBusImpl
+
+        /// <summary>
+        /// Default implementation.
+        /// </summary>
+        private class MessageBusImpl
+        {
+            private IDictionary<Type, List<MessageObserver>> _observers;
+
+            internal MessageBusImpl()
+            {
+                _observers = new Dictionary<Type, List<MessageObserver>>();
+            }
+
+            internal void Publish(Type messageType, Message message)
+            {
+                if (_observers.ContainsKey(messageType))
+                {
+                    foreach (var observer in _observers[messageType])
+                    {
+                        observer.OnNext(message);
+                    }
+                }
+            }
+
+            internal IDisposable Subscribe(Type messageType, Action<Message> onMessageReceived)
+            {
+                var messageObservers = new List<MessageObserver>();
+
+                if (!_observers.ContainsKey(messageType))
+                {
+                    _observers.Add(messageType, messageObservers);
+                }
+                else 
+                {
+                    messageObservers = _observers[messageType];
+                }
+
+                var observer = new MessageObserver(_observers[messageType], onMessageReceived);
+                messageObservers.Add(observer);
+                return observer;
+            }
+
+            internal void UnsubscribeAll(Type messageType)
+            {
+                if (_observers.ContainsKey(messageType))
+                {
+                    foreach (var observer in _observers[messageType])
+                    {
+                        observer.Dispose();
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
