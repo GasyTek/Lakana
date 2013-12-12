5a96f97515ba38f46883ce826d45086a171773b0
gasytek_cp <SND\gasytek_cp>
2013-12-12 23:51:21 +0000

    Release Lakana 2.5

--- V2/GasyTek.Lakana/WPF/GasyTek.Lakana.Mvvm/Properties/AssemblyInfo.cs
+++ V2/GasyTek.Lakana/WPF/GasyTek.Lakana.Mvvm/Properties/AssemblyInfo.cs
@@ -51,8 +51,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
 
 [assembly: InternalsVisibleTo("GasyTek.Lakana.Mvvm.Tests")]
 
